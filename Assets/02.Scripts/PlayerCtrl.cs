375d51aec2a37b2c036b76247db4a8ed31717235
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-08-02 15:33:51 +0900

    플레이어, 몬스터 죽음

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -19,6 +19,12 @@ public class PlayerCtrl : MonoBehaviour
     //현재 생명 값
     public float currHp;
 
+    //델리게이트 선언
+    public delegate void PlayerDieHandler();
+
+    //이벤트 선언
+    public static event PlayerDieHandler OnPlayerDie;
+
     IEnumerator Start()
     {
         //Hp 초기화
@@ -106,13 +112,19 @@ public class PlayerCtrl : MonoBehaviour
     {
         Debug.Log("바이~");
         
+        /*
         //MONSTER 태그를 가진 모든 게임오브젝트를 찾아옴
         GameObject[] monsters = GameObject.FindGameObjectsWithTag("MONSTER");
 
+        
         //모든 몬스터의 OnPlayDie 함수를 순차적으로 호출
         foreach (GameObject monster in monsters)
         {
             monster.SendMessage("OnPlayerDie",SendMessageOptions.DontRequireReceiver);
         }
+        */
+
+        //주인공 사망 이벤트 호출(발생)
+        OnPlayerDie();
     }
 }
